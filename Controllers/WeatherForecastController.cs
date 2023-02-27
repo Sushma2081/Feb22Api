f7b3a522f3b0847ca536289fdf06586e21a7a471
Sushma2081 <sushmarao2001@gmail.com>
2023-02-27 13:44:26 +0530

    Commit

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -27,8 +27,7 @@ public class WeatherForecastController : ControllerBase
         return await _context.Students.ToListAsync();
     }
 
-    [HttpGet(Name ="GetSubjectsdata")]
-
+    [HttpGet(Name = "GetSubjectsdata")]
     public async Task<IEnumerable<Subject>> SubjectsFetch()
     {
         return await _context.Subjects.ToListAsync();
@@ -88,10 +87,6 @@ public class WeatherForecastController : ControllerBase
         }
     }
 
-   
-
-
-
     [HttpPost]
     public async Task<IActionResult> PostMarks(Marks marks)
     {
@@ -105,14 +100,112 @@ public class WeatherForecastController : ControllerBase
         }
 
         
-        marks.StudentId = existingStudent.StudentId;
-        marks.SubjectId = existingSubject.SubjecttId;
+        marks.Student = existingStudent;
+        marks.Subject = existingSubject;
         _context.Marks.Add(marks);
         await _context.SaveChangesAsync();
 
         return Ok(CreatedAtAction("GetMarks", new { StudentId = marks.StudentId, SubjectId = marks.SubjectId }, marks));
 
+    }
 
+    [HttpPut(Name = "Updatestudent")]
+    public async Task<IActionResult> UpdateStudent(Student student)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Students.Update(student);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+        else
+        {
+            return BadRequest(ModelState);
+        }
+    }
+
+    [HttpPut(Name ="Updatesubject")]
+    public async Task<IActionResult> UpdateSubject(Subject subject)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Subjects.Update(subject); 
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+        else
+        {
+            return BadRequest(ModelState);
+        }
+    }
+
+    [HttpPut(Name = "Updatemarks")]
+    public async Task<IActionResult> Updatemarks(Marks marks)
+    {
+        /*var existingStudent = await _context.Students.FindAsync(marks.StudentId);
+        var existingSubject = await _context.Subjects.FindAsync(marks.SubjectId);
+
+        if (existingStudent == null || existingSubject == null)
+        {
+            return NotFound();
+        }*/
+
+        var existingmarks = await _context.Marks.Where(X=>X.StudentId==marks.StudentId && X.SubjectId==marks.SubjectId).FirstOrDefaultAsync();
+        existingmarks.marks = marks.marks;
+        
+        _context.Marks.Update(marks);
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
+
+
+
+    [HttpDelete(Name = "deletestudent")]
+    public async Task<IActionResult> Deletestudent(int Id)
+    {
+        var recordtobedel = _context.Students.Find(Id);
+        if (recordtobedel == null)
+        {
+            return NotFound();
+        }
+        else
+        {
+            _context.Students.Remove(recordtobedel);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+    [HttpDelete(Name = "deletesubject")]
+    public async Task<IActionResult> Deletesubject(int Id)
+    {
+        var recordtobedel = _context.Subjects.Find(Id);
+        if (recordtobedel == null)
+        {
+            return NotFound();
+        }
+        else
+        {
+            _context.Subjects.Remove(recordtobedel);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+
+    [HttpDelete(Name = "deletemarks")]
+    public async Task<IActionResult> Deletemarks(int StudentId,int SubjectId)
+    {
+        var recordtobedel = _context.Marks.Find(StudentId,SubjectId);
+        if (recordtobedel == null)
+        {
+            return NotFound();
+        }
+        else
+        {
+            _context.Marks.Remove(recordtobedel);
+            _context.SaveChanges();
+            return Ok();
+        }
     }
 
 
